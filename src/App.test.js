98ee3b05767c2248f3e62a2ee8533da751ca324f
Hammad Mansoor <hammadmansoor75@gmail.com>
2022-11-07 12:58:33 +0500

    First Commit

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,18 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByTestId('comp');
   expect(linkElement).toBeInTheDocument();
+});
+
+test('h1 displays on screen', () => {
+  render(<App />);
+  const element = screen.getByTestId('heading');
+  expect(element).toHaveTextContent('Hello World');
+});
+
+test('First element is present on screen', () => {
+  render(<App />);
+  const element = screen.getByTestId('list');
+  expect(element).toHaveTextContent('First Element');
 });
